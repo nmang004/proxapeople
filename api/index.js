ca6dc2fa61fc065f101b7e25eafa666014174e78
Nick Mangubat <nickmangubat@Nicks-Mac-mini.local>
2025-07-20 20:24:25 -0400

    Fix ES module syntax in Vercel serverless function

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
 // Vercel serverless function that serves the Express app
-const { createServer } = require('../dist/index.js')
+import { createServer } from '../dist/index.js'
 
 // Export the Express app as a Vercel serverless function
-module.exports = async (req, res) => {
+export default async (req, res) => {
   const app = await createServer()
   return app(req, res)
 }
